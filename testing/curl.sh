2bf1a6ba542de52167965c85f2061ac045f258c7
stefan.pinter@bearingpoint.com <stefan.pinter@bearingpoint.com>
2026-07-10 12:12:55 +0000

    add qwen35c9der and localmodelcache and added a curl test command

--- testing/curl.sh
+++ testing/curl.sh
@@ -1,1 +1,3 @@
 curl -H "Content-Type: application/json" https://qwen3.llm.bearingpoint.com/openai/v1/chat/completions -d @./chat-input.json
+
+curl -H "Content-Type: application/json" https://inference.llm.bearingpoint.com/kserve-qwen3coder/qwen3coder/v1/chat/completions -d @./chat-input.json
